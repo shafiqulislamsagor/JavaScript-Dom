e522087c9071f782131674a5aa4fa85a0c1d5e78
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-11-28 20:18:51 +0600

    attribute change

--- file-1/index.js
+++ file-1/index.js
@@ -4,3 +4,9 @@ for (let i = 0; i < x.length ; i++){
     text += x.elements[i].value + "<br>";
 }
 document.getElementById("demo").innerHTML = text;
+
+//attribute change:
+
+const img = document.getElementById("id");
+
+img.src = "change.img";
